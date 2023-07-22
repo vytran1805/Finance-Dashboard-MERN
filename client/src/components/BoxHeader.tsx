9bd8beff7fd6c84e41ee6dc44f8f939f0828e4f7
VyT <vytran1805@gmail.com>
2023-07-21 22:58:37 -0400

    Create reusable box header

---
+++ client/src/components/BoxHeader.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import FlexBetween from "./FlexBetween";
+import { Box, useTheme, Typography } from "@mui/material";
+
+type Props = {
+  icon?: React.ReactNode; //this is a material UI icon
+  title: string;
+  subtitle?: string;
+  sideText: string;
+};
+/**
+ * Reusable header for every box
+ * @param param0 
+ * @returns BoxHeader
+ */
+const BoxHeader = ({ icon, title, subtitle, sideText }: Props) => {
+  const { palette } = useTheme();
+  return (
+    <FlexBetween color={palette.grey[400]} margin="1.5rem 1rem 0 1rem">
+      <FlexBetween>
+        {icon}
+        <Box width="100%">
+          <Typography variant="h4" mb="-0.1rem">
+            {title}
+          </Typography>
+          <Typography variant="h6">{subtitle}</Typography>
+        </Box>
+      </FlexBetween>
+      <Typography variant='h5' fontWeight='700' color={palette.secondary[500]}>{sideText}</Typography>
+    </FlexBetween>
+  );
+};
+
+export default BoxHeader;
