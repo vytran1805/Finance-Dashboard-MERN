c16993edd0683b71f1aca543180179c4ab016319
VyT <vytran1805@gmail.com>
2023-07-23 00:46:24 -0400

    Modify comment

--- server/models/Product.js
+++ server/models/Product.js
@@ -9,59 +9,10 @@ const Schema = mongoose.Schema;
 loadType(mongoose);
 
 /**
- * Create a monthly Schema to track the monthly data
- * Every day, we will have Revenue, Expenses, Operational expenses and Non-operational expenses
- */
-const daySchema = new Schema(
-  {
-    date: String,
-    revenue: {
-      type: mongoose.Types.Currency,
-      currency: "CAD",
-      get: (v) => v / 100,
-    },
-    expenses: {
-      type: mongoose.Types.Currency,
-      currency: "CAD",
-      get: (v) => v / 100,
-    },
-  },
-  { toJSON: { getters: true } } //this is the setting so that we can use the 'get' above to grab our currencies
-);
-/**
- * Create a monthly Schema to track the monthly data
- * Every month, we will have Revenue, Expenses, Operational expenses and Non-operational expenses
- */
-const monthSchema = new Schema(
-  {
-    month: String,
-    revenue: {
-      type: mongoose.Types.Currency, //this is from the loadType we imported above
-      currency: "CAD",
-      get: (v) => v / 100, //divide the value to get the real currency value
-    },
-    expenses: {
-      type: mongoose.Types.Currency, //this is from the loadType we imported above
-      currency: "CAD",
-      get: (v) => v / 100, //divide the value to get the real currency value
-    },
-    operationalExpense: {
-      type: mongoose.Types.Currency, //this is from the loadType we imported above
-      currency: "CAD",
-      get: (v) => v / 100, //divide the value to get the real currency value
-    },
-    nonOperationalExpenses: {
-      type: mongoose.Types.Currency, //this is from the loadType we imported above
-      currency: "CAD",
-      get: (v) => v / 100, //divide the value to get the real currency value
-    },
-  },
-  { toJSON: { getters: true } } //this is the setting so that we can use the 'get' above to grab our currencies
-);
-/**
  * This will create our model
  * Note: need to provide the same info that
  * we were looking at when it came to the MODEL STRUCTURES
+ * Structure: price of the product, cost, transaction
  */
 const ProductSchema = new Schema(
   {
@@ -79,7 +30,7 @@ const ProductSchema = new Schema(
     transactions: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Transaction", //this is refering to each Transaction object that's relevant to the particular product  
+        ref: "Transaction", //this is refering to each Transaction object that's relevant to the particular product
       },
     ],
   },
